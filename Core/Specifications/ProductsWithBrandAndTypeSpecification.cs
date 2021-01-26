6d6d108a7ee338bfbf6325725363a7039449e9f5
Kübra <kubraakesici@gmail.com>
2021-01-26 13:12:50 +0300

    End of Section 5

---
+++ Core/Specifications/ProductsWithBrandAndTypeSpecification.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Linq.Expressions;
+using Core.Entities;
+
+namespace Core.Specifications
+{
+    public class ProductsWithBrandAndTypeSpecification : BaseSpecification<Product>
+    {
+        public ProductsWithBrandAndTypeSpecification()
+        {
+            AddIncludes(p => p.ProductBrand);
+            AddIncludes(p => p.ProductType);
+        }
+
+        public ProductsWithBrandAndTypeSpecification(int id) : base(p => p.Id == id)
+        {
+            AddIncludes(p => p.ProductBrand);
+            AddIncludes(p => p.ProductType);
+        }
+    }
+}
